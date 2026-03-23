b0be1524e0048db038030eaccf2ccf5f671200f5
hansoho <48212912+hansoh0@users.noreply.github.com>
2026-03-23 05:05:38 -0400

    Refactor AES encryption/decryption and add usage
    Refactor encryption and decryption functions, add usage instructions, and implement key derivation using PBKDF2.

--- src/main.c
+++ src/main.c
@@ -1,16 +1,23 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/aes.h>
 #include <openssl/rand.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
 
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
 #define AES_BLOCK_SIZE 16
+#define KEY_LEN 32
+#define SALT_LEN 16
+#define PBKDF2_ITR 100000
 
 // Prototypes
-unsigned char *encryptBuffer(unsigned char *buffer, long filelen, unsigned char *userKey, unsigned char *iv);
-unsigned char *decryptBuffer(unsigned char *buffer, long filelen, unsigned char *userKey, unsigned char *iv);
+unsigned char *encryptBuffer(unsigned char *buffer, long len, unsigned char *key, unsigned char *iv);
+unsigned char *decryptBuffer(unsigned char *buffer, long len, unsigned char *key, unsigned char *iv);
 
 /* Returns the required buffer size for proper AES-CBC encryption by calculating the smalles multiple of AES_BLOCK_SIZE that can fit
  * len which is the length of the buffer to be encrypted.
@@ -18,125 +25,43 @@ unsigned char *decryptBuffer(unsigned char *buffer, long filelen, unsigned char 
  * This is not a padding scheme implementation -> NOT PKCS#7
  * The buffer is zero padded via calloc -> ie. if buffer is 15 bytes, the 16th byte would be 0x00
  * -> zero padding means if last byte in data is 0x00, after decryption the end is mixed with the zero padding
- * -> ie. [01 02 03 04 05 06 07 08 09 0a 0b 0c 0e 0f 00](15 bytes) + [00](padding)
- * -> decrypted as: [01 02 03 04 05 06 07 08 09 0a 0b 0c 0e 0f 00 00] <- ambiguous
+ * -> ie. [01 02 03 04 05 06 07 08 09 0a 0b 0c 0e 0f 00](15 bytes) + [00](padding) 
+ * -> decrypted as: [01 02 03 04 05 06 07 08 09 0a 0b 0c 0e 0f 00 00] <- ambiguous 
  */
 long pad_length(long len) {
-        return ((len + AES_BLOCK_SIZE - 1) / AES_BLOCK_SIZE) * AES_BLOCK_SIZE;
+	return ((len + AES_BLOCK_SIZE - 1) / AES_BLOCK_SIZE) * AES_BLOCK_SIZE;
 }
 
 /*
- * Program to test encryption/decryption of a specified file.
- *
- * This is a proof of concept skeleton not to be used in production.
- *
- * 1. Defines a file test/file.txt
- * 2. Allows user to supply key
- * 3. Encrypts file as a binary buffer & writes out with IV
- *  OUT (test/file.enc) -> [16 Byte IV][ENCRYPED BUFFER]
- * 4. Reads test/file.enc as binary buffer
- * 5. Parses IV to use for decryption
- * 6. Decrypts buffer and writes out as decrypted binary buffer
- *  OUT (test/file.dec) -> [DECRYPTED_TEXT]
+ * usage():
+ * Displays how to use the program
  */
-int main() {
-        // Target file to encrypt decrypt
-        const char filename[] = "test/file.txt";
-        FILE *fileptr;
-        unsigned char* buffer;
-        long filelen;
+static void usage(const char *prog) {
+	fprintf(stderr,
+			"Usage: %s <mode> <path> <secret> <salt>\n"
+			" mode : encrypt | decrypt\n"
+			" path : file or directory to encrypt\n"
+			" secret : passphrase to encrypt/decrypt\n"
+			" salt : salt to add to encryption (16 bytes)\n"
+			"Example:\n"
+			" %s encrypt ./docs secret 001256648903222efff0000000000000\n",
+			prog,  prog);
+}
 
-        /*
-         * Takes key from user.
-         * This uses raw input - in prod KDF should be used.
-         * This is NOT secure, only PoC.
-         */
-        unsigned char userKey[32];
-        char input[64];
-        printf("Enter key: ");
-        fgets(input, sizeof(input), stdin);
-        memset(userKey, 0, 32);
-        memcpy(userKey, input, strlen(input) > 32 ? 32 : strlen(input));
-
-        // Open File as binary data
-        fileptr = fopen(filename, "rb");
-        if (fileptr == NULL)
-        {
-                fprintf(stderr, "Error reading from %s\n", filename);
-                return 1;
-        }
-
-        // Retreive file size
-        fseek(fileptr, 0, SEEK_END);
-        filelen = ftell(fileptr);
-        rewind(fileptr);
-
-        // Allocate memory for 1 byte per char
-        long padded_len = pad_length(filelen);
-        buffer = calloc(padded_len, 1);
-        if (buffer == NULL) {
-                fclose(fileptr);
-                return 1;
-        }
-
-        // Read file into buffer 1 byte per char
-        fread(buffer, 1, filelen, fileptr);
-
-        // Close file
-        fclose(fileptr);
-
-        // Generatie IV
-        unsigned char iv[AES_BLOCK_SIZE];
-        RAND_bytes(iv, AES_BLOCK_SIZE);
-
-        // Copy unmutated IV for storage
-        unsigned char iv_enc[AES_BLOCK_SIZE];
-        memcpy(iv_enc, iv, AES_BLOCK_SIZE);
-
-        // Encrypt buffer
-        unsigned char *encrypted_buffer = encryptBuffer(buffer, padded_len, userKey, iv_enc);
-
-        // Open file for writing encrypted text
-        FILE *encfp_wb = fopen("test/file.enc", "wb");
-        if (encfp_wb == NULL) {
-                perror("Error Opening File");
-                return 1;
-        }
-
-        // Write IV to the first 16 bytes, then write encryped text
-        fwrite(iv, 1, AES_BLOCK_SIZE, encfp_wb);
-        fwrite(encrypted_buffer, 1, padded_len, encfp_wb);
-        fclose(encfp_wb);
-
-
-        // Open file for reading encrypted text
-        FILE *encfp_rb = fopen("test/file.enc", "rb");
-        if (encfp_rb == NULL) return 1;
-
-        // Read IV from first 16 bytes of the file
-        unsigned char read_iv[AES_BLOCK_SIZE];
-        fread(read_iv, 1, AES_BLOCK_SIZE, encfp_rb);
-
-        // Read encrypted text from file
-        unsigned char *enc_input = malloc(padded_len);
-        fread(enc_input, 1, padded_len, encfp_rb);
-        fclose(encfp_rb);
-
-        // Decrypt encrypted buffer
-        unsigned char *decrypted_buffer = decryptBuffer(enc_input, padded_len, userKey, read_iv);
-
-        // Open file for writing decrypted text
-        FILE *decfp_wb = fopen("test/file.dec", "wb");
-        fwrite(decrypted_buffer, 1, padded_len, decfp_wb);
-        fclose(decfp_wb);
-
-        // Free all allocated memory
-        free(buffer);
-        free(encrypted_buffer);
-        free(enc_input);
-        free(decrypted_buffer);
-
-        return 0;
+/*
+ * derive_key():
+ * Derives a fixed length key from a user secret using PBKDF2
+ * 
+ * char secret: User supplied password
+ * char salt: random salt
+ * char key_out: derived key buffer 
+ * 
+ * salt mitigates rainbow table attacks, PBKDF2_ITR prevents brute force
+ *
+ * https://docs.openssl.org/3.0/man3/PKCS5_PBKDF2_HMAC/#name
+ */
+static int derive_key(const char *secret, const unsigned char *salt, unsigned char *key_out) {
+       return PKCS5_PBKDF2_HMAC(secret, (int)strlen(secret), salt, SALT_LEN, PBKDF2_ITR, EVP_sha256(), KEY_LEN, key_out); 
 }
 
 /* Buffer encryption AES-256-CBC
@@ -144,12 +69,13 @@ int main() {
  * IV is modified during this phase so storage of IV must happen before this
  * Returns a newly allocated buffer with encrypted data
  */
-unsigned char *encryptBuffer(unsigned char *buffer, long filelen, unsigned char *userKey, unsigned char *iv) {
-        AES_KEY aes_key;
-        AES_set_encrypt_key(userKey, 256, &aes_key);
-        unsigned char* encrypted_buffer = malloc(filelen);
-        AES_cbc_encrypt(buffer, encrypted_buffer, filelen, &aes_key, iv, AES_ENCRYPT);
-        return encrypted_buffer;
+unsigned char *encryptBuffer(unsigned char *buffer, long len, unsigned char *key, unsigned char *iv) {
+	AES_KEY aes_key;
+	AES_set_encrypt_key(key, 256, &aes_key);
+	unsigned char* encrypted_buffer = malloc(len);
+	if (!encrypted_buffer) return NULL;
+	AES_cbc_encrypt(buffer, encrypted_buffer, len, &aes_key, iv, AES_ENCRYPT);
+	return encrypted_buffer;
 }
 
 /* Buffer decryption AES-256-CBC
@@ -159,11 +85,14 @@ unsigned char *encryptBuffer(unsigned char *buffer, long filelen, unsigned char 
  * This is NOT secure, only PoC
  * Returns a newly alllocated buffer with decrypted data.
  */
-unsigned char *decryptBuffer(unsigned char *buffer, long filelen, unsigned char *userKey, unsigned char *iv) {
-        AES_KEY aes_key;
-        AES_set_decrypt_key(userKey, 256, &aes_key);
-        unsigned char *decrypted_buffer = malloc(filelen);
-        AES_cbc_encrypt(buffer, decrypted_buffer, filelen, &aes_key, iv, AES_DECRYPT);
-        return decrypted_buffer;
+unsigned char *decryptBuffer(unsigned char *buffer, long len, unsigned char *key, unsigned char *iv) {
+	AES_KEY aes_key;
+	AES_set_decrypt_key(key, 256, &aes_key);
+	unsigned char *decrypted_buffer = malloc(len);
+	if (!decrypted_buffer) return NULL;
+	AES_cbc_encrypt(buffer, decrypted_buffer, len, &aes_key, iv, AES_DECRYPT);
+	return decrypted_buffer;
 }
 
+int main(int argc, char *argv[]) {
+}
