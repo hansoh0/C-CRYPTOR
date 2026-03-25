554d38eb413cd73d6a0a560139e5db3774fea9c0
hansoho <48212912+hansoh0@users.noreply.github.com>
2026-03-25 08:03:21 -0400

    Add key derivation and file encryption functions

--- src/crypt.h
+++ src/crypt.h
@@ -7,5 +7,8 @@
 
 long pad_length(long len);
 int zero_file(const char *input_path, long filelen);
+int derive_key(const char *secret, const unsigned char *salt, unsigned char *key_out);
+int encrypt_file(const char *input_path, const char *output_path, const unsigned char *key);
+
 
 #endif 
